22bf588fd81062e7feab8621c3c0715d2a5bc401
JoaoACastro <joaoaccastro@gmail.com>
2024-10-15 18:29:16 -0300

    update styles

--- src/script/engine.js
+++ src/script/engine.js
@@ -16,9 +16,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const difficulty = localStorage.getItem("difficulty");
   if (difficulty) {
     loadScript(`src/script/engine${difficulty}.js`);
+
+    const gameScreen = document.getElementById("gameScreen");
+
+    gameScreen.classList.remove("easyGame","normalGame","hardGame");
+    
+    if(difficulty ==="Easy"){
+        gameScreen.classList.add("easyGame");
+    } else if (difficulty === "Normal"){
+        gameScreen.classList.add("normalGame");
+    } else if (difficulty === "Hard"){
+        gameScreen.classList.add("hardGame");
+    }
+
     localStorage.removeItem("difficulty"); // Limpar o local storage após carregar o script
   }
 });
+
 
 // Event listeners para os botões
 document.getElementById("easyButton").addEventListener("click", function () {
@@ -29,4 +43,5 @@ document.getElementById("normalButton").addEventListener("click", function () {
 });
 document.getElementById("hardButton").addEventListener("click", function () {
   resetPage("Hard");
+  document.getElementById("game").classList.add("hardGame");
 });
